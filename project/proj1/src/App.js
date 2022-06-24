e9fbdd2118bb0a15256a6c8d59dd82990d7616cc
oss <ossi0077@naver.com>
2022-06-24 14:15:19 +0900

    0624 학습

--- project/proj1/src/App.js
+++ project/proj1/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Box1 from './component/Box1';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World!</div>
+    <div className='basic'>
+      <Box1 name="한국"/>
+      <Box1 name="미국"/>
+      <Box1 name="중국"/>
     </div>
   );
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# React 학습
+## 2022/06/24
+react 환경 구성, component 실습
