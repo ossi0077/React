c9136479b458b53d491c1f38c1924e0644ec9679
oss <ossi0077@naver.com>
2022-06-27 15:42:21 +0900

    0627 학습

--- project/proj1/src/App.js
+++ project/proj1/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Box1 from './component/Box1';
+import {useState} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increase = ()=>{
+    setCounter(counter + 1);
+  }
+  const decrease = ()=>{
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className='basic'>
+    <div>
       <Box1 name="한국"/>
       <Box1 name="미국"/>
       <Box1 name="중국"/>
+
+      <div>{counter}</div>
+      <button onClick={increase}>+1</button>
+      <button onClick={decrease}>-1</button>
     </div>
   );
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # React 학습
 ## 2022/06/24
 react 환경 구성, component 실습
+## 2022/06/27
+storybook 실습
